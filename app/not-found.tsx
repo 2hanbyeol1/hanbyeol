04d80ee2bf95c93882742a43ff2fc29b1704904c
2hanbyeol1 <2hanbyeol1@naver.com>
2023-12-10 03:29:22 +0900

    design: not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,15 @@
 export default function NotFound() {
   return (
     <div>
-      <h1>한별 포트폴리오</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="w-96 border-b border-gray-300 pb-3 text-xl text-center font-semibold">
+          HanbyeoL⭐
+        </div>
+        <div className="py-3">페이지를 찾을 수 없습니다.</div>
+        <a className="bg-white text-black px-3 py-1 rounded" href="/">
+          포트폴리오 보러가기
+        </a>
+      </div>
     </div>
   )
 }
