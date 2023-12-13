2b4a385fc53d1f8b84b6406305a1a7d93198ea21
2hanbyeol1 <2hanbyeol1@naver.com>
2023-12-13 22:15:44 +0900

    design: header z-index

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -2,7 +2,7 @@ const headerItems = ['About', 'Skills', 'Projects', 'Career']
 
 export default function Header() {
   return (
-    <header className="bg-black fixed top-0 flex justify-between items-center w-screen px-5 h-14 border-b border-gray-300">
+    <header className="z-50 bg-black fixed top-0 flex justify-between items-center w-screen px-5 h-14 border-b border-gray-300">
       <span className="text-xl font-semibold">HanbyeoL⭐</span>
       <ul className="flex">
         {headerItems.map((item) => {
