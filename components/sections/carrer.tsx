2562dfb8a0fe538dad5ec7bdf8704bae4f1b4881
2hanbyeol1 <2hanbyeol1@naver.com>
2024-01-27 23:09:22 +0900

    [fix] deploy errors

--- components/sections/carrer.tsx
+++ components/sections/carrer.tsx
@@ -1,10 +1,10 @@
-import Title from '../common/title'
+import Title from '../common/title';
 
 export default function Career() {
   return (
     <>
       <Title title="Career" />
-      <div className="flex">There's no careers yet 🥰😳</div>
+      <div className="flex">There&apos;s no careers yet 🥰😳</div>
     </>
-  )
+  );
 }
